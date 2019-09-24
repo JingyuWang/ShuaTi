8dbde1be1cc65dcc716a641af5f783c304e55d7a
Arthur Wang <v-wanji@microsoft.com>
2019-09-23 17:38:29 -0700

    Project Initialization code part

---
+++ RoundOne/RoundOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoundOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
