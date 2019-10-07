4be8db8ad4201525bb1d13d74d816442aa5a001d
Arthur Wang <v-wanji@microsoft.com>
2019-10-07 15:41:30 -0700

    Configure the repo ignore path

--- RoundOne/RoundOne/Program.cs
+++ RoundOne/RoundOne/Program.cs
@@ -1,12 +1,123 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace RoundOne
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char[][] board = new char[][]
+            {
+                new char[] { '1', '1' ,'0', '0', '0'},
+                new char[] { '1', '1', '0', '0', '0' },
+                new char[] { '0', '0', '1', '0', '0' },
+                new char[] { '0', '0', '0', '1', '1' }
+            };
+            Queue<Index> queue = new Queue<Index>();
+            int count = 0;
+            for(int i = 0; i < board.Length; i++)
+            {
+                for (int j=0; j < board[i].Length; j++)
+                {
+                    if (board[i][j] == '1')
+                    {
+                        Console.WriteLine("k is {0}: l is {1}", i, j);
+                        count++;
+                        Index po = new Index(i, j);
+                        queue.Enqueue(po);
+                        while (queue.Count > 0)
+                        {
+                            Index temp = queue.Dequeue();
+                            //Console.WriteLine("Queue length is {0}", queue.Count);
+                            int k = temp.X, l = temp.Y;
+                            //Console.WriteLine("k is {0}: l is {1}", k, l);
+                            board[k][l] = '2';
+                            //Console.WriteLine(board[k][l]);
+
+                            if (k + 1 < board.Length && board[k + 1][l] == '1')
+                            {
+                                queue.Enqueue(new Index(k + 1, l));
+                                Console.WriteLine("#");
+                            }                                
+                            if (l + 1 < board[k].Length && board[k][l + 1] == '1') { 
+
+                                queue.Enqueue(new Index(k, l + 1));
+                                Console.WriteLine("##");
+                            }
+                            if (k - 1 >= 0 && board[k-1][l] == '1')
+                            {
+                                queue.Enqueue(new Index(k - 1, l));
+                                Console.WriteLine("###");
+                            }
+                                
+                            if (l - 1 >= 0 && board[k][l - 1] == '1')
+                            {
+                                queue.Enqueue(new Index(k, l - 1));
+                                Console.WriteLine("####");
+                            }
+                               
+                            Thread.Sleep(300);
+                        }
+                    }
+                }
+            }
+
+            foreach(var item in board)
+            {
+                foreach(var s in item)
+                {
+                    Console.Write(s);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Total island is {0}", count);
+        }
+        public class Index
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public Index(int a, int b)
+            {
+                X = a;
+                Y = b;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns>string array</returns>
+        public static string[] GetStringListFromString(string t)
+        {
+            var dd = t.Replace("[", string.Empty).Replace("]",string.Empty).Replace(" ", string.Empty);
+            return dd.Split(",");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns>int array</returns>
+        public static int[] GetIntListFromString(string t)
+        {
+            try
+            {
+                var dd = t.Replace("[", string.Empty).Replace("]", string.Empty).Replace(" ", string.Empty);
+                return Array.ConvertAll(dd.Split(","), int.Parse);
+            }
+            catch
+            {
+                Console.WriteLine("Convertion error.");
+                return null;
+            }
+
         }
     }
 }
---
+++ RoundOne/RoundOne/Zhongdeng/200. Number of Islands.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution
+{
+    public class _200
+    {
+        public static int NumIslands(char[][] grid)
+        {
+            int count = 0;
+            for(int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j] == '1')
+                    {
+                        count++;
+                        Queue<Index> q = new Queue<Index>();
+                        q.Enqueue(new Index(i, j));
+                        while (q.Count > 0)
+                        {
+                            Index temp = q.Dequeue();
+                            int k = temp.X, l = temp.Y;
+                            grid[k][l] = '2';
+                            if (k + 1 < grid.Length && grid[k + 1][l] == '1')
+                            {
+                                q.Enqueue(new Index(k + 1, l));
+                                Console.WriteLine("#");
+                            }
+                            if (l + 1 < grid[k].Length && grid[k][l + 1] == '1')
+                            {
+
+                                q.Enqueue(new Index(k, l + 1));
+                                Console.WriteLine("##");
+                            }
+                            if (k - 1 >= 0 && grid[k - 1][l] == '1')
+                            {
+                                q.Enqueue(new Index(k - 1, l));
+                                Console.WriteLine("###");
+                            }
+
+                            if (l - 1 >= 0 && grid[k][l - 1] == '1')
+                            {
+                                q.Enqueue(new Index(k, l - 1));
+                                Console.WriteLine("####");
+                            }
+                        }
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        public class Index
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public Index(int a, int b)
+            {
+                X = a;
+                Y = b;
+            }
+        }
+    }
+}
+
+
+/*
+ Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. 
+ An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
+ You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+Input:
+11110
+11010
+11000
+00000
+
+Output: 1
+
+Example 2:
+
+Input:
+11000
+11000
+00100
+00011
+
+Output: 3*/
